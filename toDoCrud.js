d67fb8086bc2f30d67ab1078dd3c7eecba8fd352
rahul <rahulm@webelight.co.in>
2024-01-05 11:51:30 +0530

    modify changes in js filess

--- toDoCrud.js
+++ toDoCrud.js
@@ -1,19 +1,21 @@
 // fetching an element
 
-const inputName = document.getElementById("taskInput");
-const inputAge = document.getElementById("taskInput2");
-const tableBody = document.getElementById("tableBody")
 
+const tableBody = document.getElementById("tableBody")
 // Function to add data
 function addData() {
-  const name = nameInput.value;
-  const age = ageInput.value;
-
+  const inputName = document.getElementById("taskInput").value;
+const inputAge = document.getElementById("taskInput2").value;
+  console.log(inputName,"inputName");
+  const name = inputName.innerText;
+  const age = inputAge.value;
+debugger
   if (name && age) {
     const row = tableBody.insertRow();
     const cell1 = row.insertCell(0);
     const cell2 = row.insertCell(1);
     const cell3 = row.insertCell(2);
+    cell1.innerHTML = name;
   }
  
   else {
@@ -21,9 +23,9 @@ function addData() {
   }
 }
 
-cell1.innerHTML = name;
-    cell2.innerHTML = age;
+// cell1.innerHTML = name;
+//     cell2.innerHTML = age;
     
     
-    nameInput.value = '';
-    ageInput.value = '';
+//     nameInput.value = '';
+//     ageInput.value = '';
